46bcdf545be804f7dd232c639c088d54b5cb4d16
bookhabit <qnrgoqlt@naver.com>
2024-05-29 21:51:38 +0900

    custom header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import { Link, Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
 
 const Page = () => {
+  const [category,setCategory] = useState('Tiny homes')
+  const onDataChanged = (category:string)=>{
+    console.log('CHANGED 카테고리 : ',category)
+    setCategory(category)
+  }
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Booking</Link>
-      <Link href={"/listings/123"}>Listing Details</Link>
+    <View style={{ flex: 1, marginTop: 160 }}>
+      <Stack.Screen
+        options={{
+          header:()=><ExploreHeader onCategoryChanged={onDataChanged}  />
+        }}
+      />
+      <Listings listings={[]} category={category} />
     </View>
   )
 }
